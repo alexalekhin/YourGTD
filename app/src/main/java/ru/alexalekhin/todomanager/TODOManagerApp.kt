9b4907560e9d003fd977d8664061935384712398
Aleksamdr <alekh.alvl@gmail.com>
2020-07-24 22:28:59 +0300

    dependencies: update library versions, Kotlin version and etc.

--- app/src/main/java/ru/alexalekhin/todomanager/TODOManagerApp.kt
+++ app/src/main/java/ru/alexalekhin/todomanager/TODOManagerApp.kt
@@ -1,8 +1,6 @@
 package ru.alexalekhin.todomanager
 
 import android.app.Application
-import com.facebook.stetho.Stetho
-
 import ru.alexalekhin.todomanager.di.DaggerTODOAppComponent
 import ru.alexalekhin.todomanager.di.TODOAppComponent
 import ru.alexalekhin.todomanager.di.modules.ContextModule
@@ -14,6 +12,5 @@ class TODOManagerApp : Application() {
         component = DaggerTODOAppComponent.builder()
             .contextModule(ContextModule(this))
             .build()
-        Stetho.initializeWithDefaults(this)
     }
 }
